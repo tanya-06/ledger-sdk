eb4ffb9b12541a54310d30e139933e165fcacf32
Ankur <ankur.singh@agrevolution.in>
2022-08-05 12:10:12 +0530

    added lang support

--- lib-ledger/src/main/java/lib/dehaat/ledger/presentation/ledger/ui/component/HeaderTotalOutstanding.kt
+++ lib-ledger/src/main/java/lib/dehaat/ledger/presentation/ledger/ui/component/HeaderTotalOutstanding.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import lib.dehaat.ledger.R
@@ -72,7 +73,7 @@ fun HeaderTotalOutstanding(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "Total Outstanding",
+                    text = stringResource(id = R.string.total_outstanding),
                     style = text14Sp(textColor = ledgerColors.CtaColor)
                 )
                 if (isLmsActivated()) {
