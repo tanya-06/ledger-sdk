fa6e302a3b3b6ab71b19b08edf2ce82e4a2d9cd8
Shivam Bhusri <shivambhusri12@gmail.com>
2023-08-04 16:28:44 +0530

    [FTS-2220] Fix number picker crash

--- src/main/java/lib/dehaat/ledger/presentation/ledger/downloadledger/state/DownloadLedgerViewModelState.kt
+++ src/main/java/lib/dehaat/ledger/presentation/ledger/downloadledger/state/DownloadLedgerViewModelState.kt
@@ -4,12 +4,13 @@ import lib.dehaat.ledger.presentation.model.downloadledger.DateRange
 import lib.dehaat.ledger.util.getCurrentMonth
 import lib.dehaat.ledger.util.getCurrentYear
 import lib.dehaat.ledger.util.getMonthList
+import lib.dehaat.ledger.util.getYearsList
 
 data class DownloadLedgerViewModelState(
 	val selectedDateType: String = "",
 	val showDatePicker: Boolean = false,
 	val monthList: List<String> = getMonthList(),
-	val yearsList: List<String> = emptyList(),
+	val yearsList: List<String> = getYearsList(0),
 	val yearsRangeList: List<DateRange> = emptyList(),
 	val selectedMonth: Int = getCurrentMonth(),
 	val selectedYear: Int = getCurrentYear(),
